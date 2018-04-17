7d492a2829e5b86da59dc67dbfea139c4957fe77
Koutarouu <Kiritsugufel@gmail.com>
2018-04-16 21:55:18 -0500

    Seccion 11 terminada - CRUD - Imagenes

--- app.js
+++ app.js
@@ -16,8 +16,12 @@ app.use(bodyParser.json())
 // Importar Rutas
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var hospitalRoutes = require('./routes/hospital');
+var medicoRoutes = require('./routes/medico');
 var loginRoutes = require('./routes/login');
-
+var busquedaRoutes = require('./routes/busqueda');
+var imagenesRoutes = require('./routes/imagenes');
+var uploadRoutes = require('./routes/upload');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res )=> {
@@ -26,11 +30,22 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', ( err, res )
 
   console.log('Nase de datos: \x1b[32m%s\x1b[0m','online');
 
-})
+});
+
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
+
 
 // Rutas
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital', hospitalRoutes);
+app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
 app.use('/', appRoutes);
 
 
